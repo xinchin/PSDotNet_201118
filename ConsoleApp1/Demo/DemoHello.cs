962cb7182b4b252245c9e4b371dbdbe55eca96c2
Nelson.Huang <xinchin@gmail.com>
2020-11-18 15:28:20 +0800

    Console 增加巢狀選單的方式呈現

--- ConsoleApp1/Demo/DemoHello.cs
+++ ConsoleApp1/Demo/DemoHello.cs
@@ -8,8 +8,59 @@ namespace ConsoleApp1.Demo
 {
     class DemoHello
     {
-        public static void Run() {
-            Console.WriteLine("------------ Hello World -------------------");
+        public static void Run()
+        {
+            bool canExecute = true;
+            string op = string.Empty;
+
+            //Console.WriteLine("------------ Hello World -------------------");
+            while (canExecute)
+            {
+                ShowOptions();
+                op = Console.ReadLine();
+                Console.WriteLine();
+                Console.WriteLine("Your selection is： {0}", op);
+                Console.WriteLine();
+                switch (op)
+                {
+                    case "0":
+                        Console.WriteLine("------------ Hello 001 ---------------");
+                        break;
+                    case "1":
+                        Console.WriteLine("------------ Hello 002 ---------------");
+                        break;
+                    case "exit":
+                        canExecute = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine();
         }
+
+        public static void ShowOptions()
+        {
+            List<string> optionList = new List<string>();
+            optionList.Add("Hello 001");    //0
+            optionList.Add("Hello 002");    //1
+
+
+            Console.WriteLine();
+            for (int i = 0; i < optionList.Count; i++)
+            {
+                Console.Write("{0}. {1}\t\t", i, optionList[i].ToString());
+                if ((i + 1) % 3 == 0) Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("please type 'exit' if you want to exit here");
+            Console.WriteLine();
+            Console.Write("Please type your selection :");
+        }
+
     }
+
+
 }
+
--- ConsoleAppVB1/Demo/DemoHello.vb
+++ ConsoleAppVB1/Demo/DemoHello.vb
@@ -1,5 +1,47 @@
 ﻿Public Class DemoHello
     Shared Sub Run()
-        Console.WriteLine("------------ Hello World -------------------")
+        Dim canExecute As Boolean = True
+        Dim op As String = String.Empty
+        While canExecute
+            ShowOptions()
+            op = Console.ReadLine()
+            Console.WriteLine()
+            Console.WriteLine("Your selection is： {0}", op)
+            Console.WriteLine()
+            Select Case op
+                Case "0"
+                    Console.WriteLine("-------------- Hello 001 ----------------")
+                Case "1"
+                    Console.WriteLine("-------------- Hello 002 ----------------")
+                Case "exit"
+                    canExecute = False
+                Case Else
+            End Select
+        End While
+
+        Console.WriteLine()
+
     End Sub
+
+    Shared Sub ShowOptions()
+        Dim optionList As New List(Of String)
+
+        optionList.Add("Hello 001") '0
+        optionList.Add("Hello 002") '1
+
+        Console.WriteLine()
+
+        For i = 0 To optionList.Count - 1
+            Console.Write("{0}. {1}" & vbTab & vbTab, i, optionList(i).ToString())
+            If ((i + 1) Mod 3 = 0) Then Console.WriteLine()
+        Next
+
+        Console.WriteLine()
+        Console.WriteLine()
+        Console.WriteLine("please type 'exit' if you want to exit here")
+        Console.WriteLine()
+        Console.Write("Please type your selection :")
+
+    End Sub
+
 End Class
