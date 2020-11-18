8cb7f0668ec5f70a677f635cdf7fc957c5843d22
Nelson.Huang <xinchin@gmail.com>
2020-11-18 14:59:24 +0800

    新增 windowForm (C#)

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void demoHelloToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Demo.DemoHelloForm frm = new Demo.DemoHelloForm();
+            frm.MdiParent = this;
+            frm.WindowState = FormWindowState.Maximized;
+            frm.Show();
+        }
+    }
+}
